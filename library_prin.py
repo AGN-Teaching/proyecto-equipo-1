52d7f939e6d91b21e1180b6ea2e5d7b550165c16
abi-sae <125155934+abi-sae@users.noreply.github.com>
2023-10-13 19:42:03 -0600

    library_prin.py

--- library_prin.py
+++ library_prin.py
@@ -1,54 +1,32 @@
-from book import Book
-from Usuarios import Usuario_Bi
-from library_aux import Library_aux
-
-
-def buscar(argument, where):
-    anser = False
-    for titulo in where:
-        if titulo == argument:
-            argument = titulo
-            anser = True
-            break
-        else:
-            anser = False
-    if not anser:
-        for titulo in where:
-            datos = where[titulo].get_id()
-            if argument == datos:
-                anser = True
-                argument = titulo
-                break
-            else:
-                anser = False
-    return anser, argument
-
-
-class Library:
     def __init__(self):
-        self.__catalogue = {}
-        self.__users = {}
-        self.__convenience_library = {}
+        self.__catalogue = {}   # Diccionario para almacenar información sobre libros.
+        self.__users = {}   # Diccionario para almacenar información sobre usuarios.
+        self.__convenience_library = {}   # Diccionario para almacenar información sobre bibliotecas auxiliares.
 
+# Función para buscar un libro en el catálogo de la biblioteca.
     def buscar_libro(self, book):
-        anser, book = buscar(book, self.__catalogue)
-        return anser, book
+        answer, book = buscar(book, self.__catalogue)
+        return answer, book
 
-    def agregar_libro(self, book_name, book_author, book_available, book_place, book_year, book_edithorial, book_pages):
+# Función para agregar un nuevo libro al catálogo de la biblioteca.
+    def agregar_libro(self, book_name, book_author, book_available, book_place, book_year, book_editorial, book_pages):
         self.__catalogue[book_name] = Book(book_name, book_author, book_available, book_place, book_year,
-                                           book_edithorial, book_pages)
+                                           book_editorial, book_pages)
 
+# Administra el catálogo (objetos).
     def anexar_simple(self, objeto):
         nombre_archivo = objeto.get_title()
         self.__catalogue[nombre_archivo] = objeto
 
-    def eliminar_libro(self, book, anser):
-        if anser:
+# Función para eliminar un libro del catálogo de la biblioteca.
+    def eliminar_libro(self, book, answer):
+        if answer:
             self.__catalogue.pop(book)
-            print(book + ' se ah eliminado correctamente.')
+            print(book + ' se ha eliminado correctamente.')
         else:
-            print(book + ' no se ah podido eliminar')
+            print(book + ' no se ha podido eliminar')
 
+# Función para mostrar el catálogo de libros.
     def get_catalogo(self):
         for titulos, book in self.__catalogue.items():
             print(titulos)
@@ -57,19 +35,23 @@ class Library:
                 print(f'{key} {valor}')
             print('')
 
-    def agregar_usuario(self, user_name, user_last_name, user_edge, user_adress):
-        self.__users[user_name] = Usuario_Bi(user_name, user_last_name, user_edge, user_adress)
+# Función para agregar un usuario en el catálogo de usuarios.
+    def agregar_usuario(self, user_name, user_last_name, user_edge, user_address):
+        self.__users[user_name] = Usuario_Bi(user_name, user_last_name, user_edge, user_address)
         return self.__users[user_name].get_id()
 
+# Función para buscar un usuario en el catálogo de usuarios.
     def buscar_usuario(self, user):
-        anser, user = buscar(user, self.__users)
-        return anser, user
+        answer, user = buscar(user, self.__users)
+        return answer, user
 
+# Función para hacer un préstamo a un usuario.
     def prestar_libro_a_usuario(self, book, user):
-        anser, book = buscar(book, self.__catalogue)
-        if anser:
+        answer, book = buscar(book, self.__catalogue)
+        if answer:
             while True:
-                type_ = input('1.Regular (2 semanas)\n2.Rapido (2 dias)\nSelecciona un tipo (por numero):\n')
+                # Tiempos disponibles para el préstamo.
+                type_ = input('1.Regular (2 semanas).\n2.Rápido (2 días).\nSelecciona un tipo (por número):\n')
                 if type_.isdigit():
                     type_ = int(type_)
                     self.__catalogue[book].disponibilidad(False)
@@ -79,32 +61,37 @@ class Library:
                     print('No es valido.')
                     continue
         else:
-            print('No se encontro libro.')
+            print('No se encontró el libro.')
 
+# Función para regresar un libro en préstamo de parte de un usuario.
     def regresar_libro_usuario(self, book, user):
-        anser, book = buscar(book, self.__catalogue)
-        if anser:
+        answer, book = buscar(book, self.__catalogue)
+        if answer:
             self.__catalogue[book].disponibilidad(True)
             self.__users[user].regresar_renta(book)
 
+# Función para prestar un libro a una biblioteca en convenio.
     def prestar_libro_a_biblioteca(self, book, library):
-        anser, book = buscar(book, self.__catalogue)
-        if anser:
+        answer, book = buscar(book, self.__catalogue)
+        if answer:
             self.__catalogue[book].disponibilidad(False)
             self.__convenience_library[library].agregar_rentas(book)
         else:
-            print('No se encontro libro.')
+            print('No se encontró el libro.')
 
+# Función para buscar una biblioteca en el catálogo de bibliotecas.
     def buscar_biblioteca(self, library):
-        anser, library = buscar(library, self.__convenience_library)
-        return anser, library
+        answer, library = buscar(library, self.__convenience_library)
+        return answer, library
 
-    def agregar_libreria(self, library_name, library_adrees):
-        self.__convenience_library[library_name] = Library_aux(library_name, library_adrees)
+# Función para agregar una biblioteca en el catálogo.
+    def agregar_libreria(self, library_name, library_address):
+        self.__convenience_library[library_name] = Library_aux(library_name, library_address)
         return self.__convenience_library[library_name].get_id()
 
+# Función para regresar un libro en préstamo de parte de una biblioteca.
     def regresar_libro_biblioteca(self, book, library):
-        anser, book = buscar(book, self.__catalogue)
-        if anser:
+        answer, book = buscar(book, self.__catalogue)
+        if answer:
             self.__catalogue[book].disponibilidad(True)
             self.__convenience_library[library].regresar_rentas(book)
